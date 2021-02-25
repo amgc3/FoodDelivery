6c3c6e3205a041c3753e7bcd10d9299fcea0336c
Anna <adropofwater0@gmail.com>
2021-02-25 19:15:38 +0000

    project setup

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+
+const db = mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    dbName: "FoodOrderingDB",
+  })
+  .catch((err) => console.log(err));
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(process.env.PORT, () => {
+    console.log(`API listening on http://${process.env.IP}:${process.env.PORT}`);
+  });
